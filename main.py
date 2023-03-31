e40cbb8615a6daa09d986b0397cee7d1c99407ee
Victor Gomes <victor.almeida@unigranrio.br>
2023-03-31 17:02:55 -0300

    att

--- main.py
+++ main.py
@@ -2,14 +2,51 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime as dt
+import re
 
 st.set_page_config('Destino', 'https://controllab.com/wp-content/uploads/favicon-32x32-1.png')
+ano = dt.datetime.now().year
+mes = dt.datetime.now().month
+
+def convert_num_to_text(datas):
+    mes_text = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    nova_lista = list()
+    padrao = re.compile('\d{1,2}\/')
+    for data in datas:                
+        mes = data.split('/')[0]
+        nova_data = padrao.sub(f'{mes_text[int(mes)-1]}/', data)
+        nova_lista.append(nova_data)
+    return nova_lista
+
+def datas_futuras(ano, mes):        
+    range_ano =list()
+    for i in range(1,13):
+        mes += + 1
+        if mes > 12:
+            mes =1
+            ano += 1
+        texto = f'{mes}/{ano}'
+        range_ano.append(texto)    
+    range_ano = convert_num_to_text(range_ano)    
+    return range_ano
+
+range_ano = datas_futuras(ano, mes)
+
+
 
 
 
 
 st.image('https://controllab.com/wp-content/uploads/logo_controllab_secundario_COR.png', width=200)
 st.title('Formação de kit')
+
+def pegar_itens(itens, df):
+    cols = st.columns(itens+1)
+    itens_list = [cols[i].selectbox(f'item {i+1}', 
+                               options=list(df.lote)[:itens]+[''],
+                               index=len(list(df.lote)[:itens]))for i in range(itens)]
+    cols[itens].selectbox('Rodada', options=range_ano)
+
 
 def formar_kits(tipo: str, itens: int, qtd: int, produto: str,dados):
     '''
@@ -24,10 +61,12 @@ def formar_kits(tipo: str, itens: int, qtd: int, produto: str,dados):
         intermediario = dados.loc[(dados.Int_CI >= qtd) & (dados.produto == produto)]
         if len(intermediario)>=itens:
             intermediario.sort_values('Int_CI', inplace=True)
-            st.write(intermediario.set_index('lote')[:itens])
-            st.latex(fr'''
+            
+            texto = fr'''
 Intermediário\space CI\space\to Kit \space CI
-            ''')
+            '''
+            df = intermediario.set_index('lote')[:itens]
+            return df, texto
 
         else:   
             intermediario.sort_values('Int_CI', ascending=False,inplace=True)
@@ -70,8 +109,8 @@ Intermediário\space CI\space\to Kit \space CI
                         pass
                     else:
                         st.warning(f'Os lotes {interep} serão usados do Intermediário EP \n\nOs lotes {kitci} serão usados do Kit CI')
-                        st.write(intermediario7.set_index('lote').drop(['produto', 'quantidade_base', 'quantidade_intep'], axis =1))
-                        st.latex(fr'''\\
+                        
+                        texto = (fr'''\\
             Lote(s)\space | \space {lotes_direto}\\
             Intermediário\space CI\space\to Kit \space CI
             \\
@@ -86,12 +125,14 @@ Intermediário\space CI\space\to Kit \space CI
             Kit\space CI\space\to Desmonte \space\to\space Intermediário \space CI \space\to\space Kit\space CI
             \\
                         ''')
+                        df =intermediario7.set_index('lote').drop(['produto', 'quantidade_base', 'quantidade_intep'], axis =1)
+                        return df, texto
 
                 else:            
 
                     st.warning(f'Os lotes {lotes_base} serão usados do lote do Base \n\nOs lotes {interep} serão usados do Intermediário EP')
-                    st.write(intermediario5.set_index('lote').drop(['produto', 'quantidade_base', 'quantidade_intep'], axis =1))
-                    st.latex(fr'''\\
+                    
+                    texto = (fr'''\\
         Lote(s)\space | \space {lotes_direto}\\
         Intermediário\space CI\space\to Kit \space CI
         \\
@@ -102,12 +143,14 @@ Intermediário\space CI\space\to Kit \space CI
         Lote(s)\space | \space {interep}\\
         Intermediário\space EP\space\to Rotulagem \space\to\space Intermediário \space CI \space\to\space Kit\space CI
                     ''')
+                    df = intermediario5.set_index('lote').drop(['produto', 'quantidade_base', 'quantidade_intep'], axis =1)
+                    return df, texto
             else:
 
                 st.warning(f'Os lotes {lotes_base} serão usados do lote do Base')
-                st.write(intermediario3.set_index('lote')[:itens])
+                df = intermediario3.set_index('lote')[:itens]
                 
-                st.latex(fr'''\\
+                texto = (fr'''\\
     Lote(s)\space | \space {lotes_direto if ',' in lotes_direto else texto}\\
     Intermediário\space CI\space\to Kit \space CI
     \\-------------------------------------\\
@@ -116,6 +159,7 @@ Intermediário\space CI\space\to Kit \space CI
 
 
                 ''')
+                return df, texto
     
     elif 'Kit_EP' == tipo:
         intermediario = dados.loc[(dados.Int_EP >= qtd) & (dados.produto == produto)]
@@ -145,12 +189,14 @@ destino = cols[1].selectbox('Destino', options=['', 'Kit_CI', 'Kit_EP'],)
 quantidade = cols[2].number_input('Quantidade de kits', min_value=1)
 itens = cols[3].number_input('Quantidade de itens', min_value=1)
 btn_destino = cols[0].button('Destinar', type='primary')
-
-
+df, texto = formar_kits(destino, itens, quantidade, produto, dados)
+pegar_itens(itens, df.reset_index())
 if btn_destino:
+    
     tabela = dados.drop('produto', axis=1).loc[dados.produto==produto]
-    inte = formar_kits(destino, itens, quantidade, produto, dados)
-    inte.to_csv('intermediario.csv', index=False)
+    
+    st.write(df)
+    df.to_csv('intermediario.csv', index=False)
 # try:
 #     inte = pd.read_csv('intermediario.csv')
 #     st.write(inte)
